748b6086216652b42bf0d3981b148c215b558775
timchnk <timchda@gmail.com>
2020-04-27 14:22:30 +0300

    Changed db config (moved to appsettings.json)

--- Zapchastulkin/Startup.cs
+++ Zapchastulkin/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Zapchastulkin.Models;
@@ -10,10 +11,17 @@ namespace Zapchastulkin
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=zapchastulkindb;Trusted_Connection=True;";
-            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+
+            string connection = Configuration.GetConnectionString("DefaultConnection");            
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 
             services.AddControllers();
 
